e19f34b2ca495e4029ec8c4e32505024358324a6
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-07-16 12:27:41 +0600

    About us section done

---
+++ src/components/button/index.jsx
@@ -0,0 +1,7 @@
+export const PrimaryButton  = () => {
+    return (
+      <button className=" bg-secondary text-white btn md:px-10  rounded-full">
+        More about us
+      </button>
+    );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { PrimaryButton } from "../../components/button";
+
 export const Home = () => {
   const myStyle = {
     backgroundImage:
@@ -30,7 +32,6 @@ export const Home = () => {
       </div>
 
       <div className="bg-white py-16 shadow-2xl container md:flex items-center absolute md:bottom-10 bottom-[-55%] md:left-[13%] ">
-
         <div className="mx-2">
           <label htmlFor="" className="font-bold text-gray-600">
             Check in
@@ -96,9 +97,91 @@ export const Home = () => {
         </div>
       </div>
 
-      <div className=" ">
-        asdfasd
-      </div>
+      {/* about section */}
+      <section className="mt-80 md:mt-48 container">
+        <div className="  grid grid-cols-2 md:grid-cols-7 gap-4">
+          <div className="col-span-3">
+            <div className="">
+              <img src="/images/a1.jpg" className=" rounded" alt="" />
+            </div>
+            <div className="grid grid-cols-2 mt-4 items-center gap-5">
+              <img className="rounded" src="/images/a2.jpg" alt="" />
+              <img className="rounded" src="/images/a2.jpg" alt="" />
+            </div>
+          </div>
+          {/* image  */}
+          {/* about section */}
+          <div className=" col-span-4">
+            {/* title */}
+            <span className=" text-secondary text-2xl font-roboto font-bold">
+              About Us
+            </span>
+            {/* welcome message */}
+            <h2 className=" text-third md:text-5xl text-2xl font-bold my-8">
+              Welcome to Relaxly Modern Hotel Rooms and Services
+            </h2>
+
+            {/* text */}
+            <p className="text-gray-500 text-justify font-roboto">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. Lorem Ipsum is
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book.
+            </p>
+
+            {/* hotel about counter */}
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-8">
+              <div className="p-10 shadow text-center rounded">
+                <span class=" text-secondary text-[50px] material-symbols-outlined">
+                  apartment
+                </span>
+                <h3 className=" text-2xl font-bold">500</h3>
+                <span className="text-gray-500">Rooms</span>
+              </div>
+              {/* rooms */}
+
+              <div className="p-10 shadow text-center rounded">
+                <span class=" text-secondary text-[50px] material-symbols-outlined">
+                  account_circle
+                </span>
+                <h3 className=" text-2xl font-bold">1500</h3>
+                <span className="text-gray-500">Customers</span>
+              </div>
+              {/* rooms */}
+
+              <div className="p-10 shadow text-center rounded">
+                <span class=" text-secondary text-[50px] material-symbols-outlined">
+                  local_shipping
+                </span>
+                <h3 className=" text-2xl font-bold">300</h3>
+                <span className="text-gray-500">Amenities</span>
+              </div>
+              {/* rooms */}
+
+              <div className="p-10 shadow text-center rounded">
+                <span class=" text-secondary text-[50px] material-symbols-outlined">
+                  percent
+                </span>
+                <h3 className=" text-2xl font-bold">230</h3>
+                <span className="text-gray-500">Packages</span>
+              </div>
+              {/* rooms */}
+              <div className="my-8">
+                <PrimaryButton></PrimaryButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* choose your offer */}
+      <section>
+        <h3  className="">Choose your offer</h3>
+      </section>
     </section>
   );
 };
